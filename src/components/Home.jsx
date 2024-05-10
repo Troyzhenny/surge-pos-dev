f6335379d5b3fa2166df1621abcd933f5c930164
Tevin Campbell <tevincampbell73@yahoo.com>
2024-05-10 14:28:10 -0400

    edited header + placeholder logo

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+const mainHub = () => {
+  return (
+    <>
+      <div>
+        <div className="groupOne">
+          <div className="mainSq"> Main Square</div>
+
+          <div className="threeSquares">
+            <div className="sq1">1</div>
+            <div className="sq2">2</div>
+            <div className="sq3">3</div>
+          </div>
+
+          <div className="offsetSq">
+            <div className="sm--square">1</div>
+            <div className="md--square">2</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default mainHub;
